06e984a239af47801d87d46722fecb90960246cd
Átila Rodrigues <atila.rodriguesal@gmail.com>
2024-02-13 11:07:50 -0300

    Refactor get_unique_ingredients function and add main function

--- scripts/get_unique_ingredients.py
+++ scripts/get_unique_ingredients.py
@@ -1,28 +1,34 @@
 import pandas as pd
 import json
-from pathlib import Path
 
-def read_unique_ingredients(csv_file, column_name):
-    df = pd.read_csv(csv_file)
-    unique_ingredients = set()
+def get_unique_ingredients(csv_file):
+    # Leitura do arquivo CSV
+    df = pd.read_csv(csv_file, encoding='utf-8')
+    
+    # Inicialização de um conjunto para armazenar os itens únicos
+    unique_items = set()
+    
+    # Iteração sobre cada linha do dataframe
+    for index, row in df.iterrows():
+        # Dividir a string em itens separados por vírgula e adicionar ao conjunto
+        items = row['Cleaned-Ingredients'].split(',')
+        unique_items.update(items)
+    
+    # Criar uma lista de objetos JSON com os itens únicos
+    json_objects = [{'name': item.strip()} for item in unique_items]
+    
+    return json_objects
 
-    for ingredients in df[column_name].str.strip('[]').str.replace("'", "").str.replace('"', '').str.split(", "):
-        unique_ingredients.update(ingredients)
-
-    # Convert unique ingredients to a list of dictionaries
-    ingredients_list = [{'name': ingredient} for ingredient in unique_ingredients]
-
-    return ingredients_list
-
-# Example usage:
-# Open reduced_recipes_ingredients.csv going back a folder and then into the data folder
-
-csv_file = Path('data/reduced_recipes_ingredients.csv')
-column_name = 'ingredients'
-unique_ingredients_json = read_unique_ingredients(csv_file, column_name)
-
-# Writing the JSON to a file
-with open('data/unique_ingredients.json', 'w') as outfile:
-    json.dump(unique_ingredients_json, outfile, indent=4)
-
-print("JSON file 'unique_ingredients.json' has been generated.")
+def main():
+    # Arquivo CSV de entrada
+    csv_file = 'data/cleaned_indian_food_dataset.csv'
+    
+    # Extrair os itens únicos e criar o JSON
+    unique_items = get_unique_ingredients(csv_file)
+    
+    # Escrever o JSON em um arquivo
+    with open('data/unique_ingredients.json', 'w') as json_file:
+        json.dump(unique_items, json_file, indent=2)
+    
+if __name__ == "__main__":
+    main()

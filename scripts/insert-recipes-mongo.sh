4c11924b319ddc7174edce5deb42dab9b5185bd6
Átila Rodrigues <atila.rodriguesal@gmail.com>
2024-02-13 21:16:52 -0300

    Add --headerline option to mongoimport command

--- scripts/insert-recipes-mongo.sh
+++ scripts/insert-recipes-mongo.sh
@@ -2,4 +2,5 @@ mongoimport \
    --collection='recipes' \
    --file='/tmp/data/cleaned_indian_food_dataset.csv' \
    --type=csv \
+   --headerline \
    --uri="mongodb://root:root@localhost:27017/recipes-book?authSource=admin"

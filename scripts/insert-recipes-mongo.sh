2d7600230178bd1984222955f6e90bb6652d789f
Átila Rodrigues <atila.rodriguesal@gmail.com>
2024-02-13 21:06:47 -0300

    Update mongoimport command in insert-recipes-mongo.sh

--- scripts/insert-recipes-mongo.sh
+++ scripts/insert-recipes-mongo.sh
@@ -2,5 +2,4 @@ mongoimport \
    --collection='recipes' \
    --file='/tmp/data/cleaned_indian_food_dataset.csv' \
    --type=csv \
-   --fields="name","unprocessed-ingredients","total_time_in_mins","cuisine","instructions","url","ingredients","ingredient-count","image-url","ingredient-count" \
    --uri="mongodb://root:root@localhost:27017/recipes-book?authSource=admin"

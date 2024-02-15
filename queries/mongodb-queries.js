a8e469ba2cc8f67329e369b901e20110787e5b24
Átila Rodrigues <atila.rodriguesal@gmail.com>
2024-02-15 15:07:17 -0300

    Add MongoDB queries for recipe book

---
+++ queries/mongodb-queries.js
@@ -0,0 +1,25 @@
+// Quais pratos da culinária x temos no livro de receita?
+
+// Quais pratos posso preparar em menos de x minutos?
+
+// Como preparo uma receita x?
+
+// Quais ingredientes são necessários para preparar um prato x?
+
+// Quais pratos posso preparar com menos de x ingredientes?
+
+// Quais pratos posso preparar em menos de x minutos e com menos de y ingredientes?
+
+// Quais foram os pratos favoritados pelo usuário x? 
+// [Recebe array de IDs recebidos pelo MariaDB, caso a aplicação estivesse em execução]
+
+// Dados que precisam ser gerados aleatoriamente:
+// Custo de preparo de um prato
+// Calorias
+// Dificuldade
+
+// Quais pratos posso preparar com menos de x reais?
+
+// Quais pratos posso preparar com menos de x calorias e menos de x reais?
+
+// Quais pratos posso preparar facilmente?
